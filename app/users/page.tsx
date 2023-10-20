291449034d978e668b7eaf7e1e4f5d21ec438a35
Tanay Vaswani <tanayvaswani@192.168.1.7>
2023-10-20 18:03:07 +0530

    Static vs Dynamic rendering..

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -8,14 +8,15 @@ interface User {
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     // caching is only available with fetch function...
-    // cache: "no-store", // refetch everytime 
-    next: { revalidate: 10 }, // refetch every 10 seconds 
+    cache: "no-store", // refetch everytime
+    // next: { revalidate: 10 }, // refetch every 10 seconds
   });
   const users: User[] = await res.json();
 
   return (
     <>
       <h1>Below is the list of users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <br />
       <ul>
         {users.map((user) => (
