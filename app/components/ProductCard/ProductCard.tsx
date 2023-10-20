df4f75b081ce8a0363edf3bd3ed3dd85c6319127
Tanay Vaswani <tanayvaswani@192.168.1.4>
2023-10-20 19:09:08 +0530

    TailwindCSS

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import AddToCart from "../AddToCart";
-import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div className="p-4 my-4 text-xl bg-sky-400 hover:bg-sky-700">
       <div>ProductCard</div>
       <AddToCart />
     </div>
   );
 };
-
+ 
 export default ProductCard;
