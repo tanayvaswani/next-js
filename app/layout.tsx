4fe6b0be116c606144b0cc9cc62fc1b46e4dc5a8
tanay-vaswani <vaswani.tanay9@gmail.com>
2023-10-21 14:54:44 +0530

    Root layout and Admin layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme="dark">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="dark" className="flex flex-col">
+      <body className={inter.className}>
+        <div className="flex p-4 m-4 justify-center space-x-4">
+          <h3>
+            <Link href="/">Home</Link>
+          </h3>
+          <h3>
+            <Link href="/users">Users</Link>
+          </h3>
+          <h3>
+            <Link href="/admin">Admin</Link>
+          </h3>
+        </div>
+        <div>{children}</div>
+      </body>
     </html>
   );
 }
