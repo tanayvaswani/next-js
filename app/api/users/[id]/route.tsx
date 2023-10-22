8d779a79a2ba308771de3d3a6161a83223dbec2d
tanay-vaswani <vaswani.tanay9@gmail.com>
2023-10-22 23:35:12 +0530

    PUT api endpoint

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -14,3 +14,23 @@ export function GET(
   }
   return NextResponse.json({ id: 1, name: "Tanay" });
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Validate req body
+  // fetch user with ID
+  // if invalid body => 400
+  // if user doesn't exists => 404
+  // update the user => updated user
+
+  const body = await request.json();
+  if (!body.name || !body.age) {
+    return NextResponse.json({ error: "Invalid age or name" }, { status: 400 });
+  }
+  if (params.id > 10) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+  return NextResponse.json({ id: 1, name: body.name });
+}
